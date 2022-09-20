97fc1237eda05915c32aa6ae56922f308b5aa736
Carlo <kys36201@gmail.com>
2022-09-20 17:13:03 +0200

    BaseballGame class 구현

--- include/BaseballGame.h
+++ include/BaseballGame.h
@@ -1,5 +1,25 @@
 #pragma once
+#include <string>
+
+using namespace std;
+
 class BaseballGame
 {
+public:
+    bool isGameDone();
+    void initialize();
+    int getNumOfStrike();
+    void input();
+    void judge();
+    void output();
 
+private:
+    void generate3DigitRandomNumber();
+
+private:
+    string answerNumberString;
+    string userNumberString;
+    int numOfStrike;
+    int numOfBall;
+    bool isGameDone;
 };
